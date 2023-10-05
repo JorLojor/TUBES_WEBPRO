40ffa009ad846b6b51427cf1350ac4d734c59572
JorLojor <nugas123man@gmail.com>
2023-10-05 10:05:46 +0700

    initiate

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,21 @@
+import './LandingPage.css';
+const LandingPage = () => {
+
+    return(
+        <>
+            <div className="container-fluid container-landing-page">
+                p
+                <div className="row">
+                    <div className="col-6">
+                        test
+                    </div>
+                    <div className="col-6">
+                        test
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage;
