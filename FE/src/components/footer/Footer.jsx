b185c2443ac00aee8424eac79a9d438b34b20ca8
JorLojor <nugas123man@gmail.com>
2023-10-31 23:07:10 +0700

    settdah

---
+++ FE/src/components/footer/Footer.jsx
@@ -0,0 +1,49 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+    
+            <footer className="row text-light mt-5 set-footer">
+                <div className="col-12 col-md-3">
+                    <div className="text-logo-footer ">
+                        <h1>CrowdLab</h1>
+                        <p className='mt-4'>Copyright © 2023.<br/>
+                            All Rights Reserved.</p>
+                    </div>
+                </div>
+
+
+                <div className="col-12 col-md-2">
+                    
+                    <ul className='page-menus-footer'>
+                        <li><h3>Page Menus</h3></li>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact</li>
+                        <li>My account</li>
+                    </ul>
+                </div>
+                <div className="col-12 col-md-2">
+                <ul className='page-menus-footer'>
+                        <li><h3>Page Menus</h3></li>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact</li>
+                        <li>My account</li>
+                    </ul>
+                </div>
+                <div className="col-12 col-md-2">
+                <ul className='page-menus-footer'>
+                        <li><h3>Page Menus</h3></li>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact</li>
+                        <li>My account</li>
+                    </ul>
+                </div>
+            </footer>
+
+    )
+}
+
+export default Footer;
--- FE/src/pages/LandingPage/IntroLP/intro.jsx
+++ FE/src/pages/LandingPage/IntroLP/intro.jsx
@@ -1,5 +1,7 @@
 import './intro.css';
+import Footer from '../../../components/footer/Footer'
 import gambar from '../../../assets/image-intro-lp.png';
+
 
 
 const IntroLP = () => {
@@ -68,7 +70,7 @@ const IntroLP = () => {
 
                 <div className="set-row set-row-trird">
                    <div className="row justify-content-between pe-5">
-                        <div className="col-1 set-categori-textt">Category</div>
+                        <div className="col-1 set-categori-textt"><span>Category</span></div>
                         
                    </div>
                    <div className="row justify-content-between set-inner-row">
@@ -104,39 +106,40 @@ const IntroLP = () => {
                 <div className="set-row set-row-four">
 
                     <div className="cover-features-title">
-                        <p>Featured</p>
+                        <span>Featured</span>
                         <h1>Featured Project</h1>
                         <p>We are a crowdfunding company founded to help startups
                             who want to start their<br/> business to accelerate their
                             achievements
                         </p>
                     </div>
-                    <div className="row justify-content-between">
-                        <div className="col-12 col-md-3  ">
+                    <div className="row ms-2 mt-5 justify-content-between">
+
+                        <div className="col-12 col-md-4 mt-5 ">
 
                             <div className="box-content-features">
-                                <div className="image-content-features" style={{backgroundImage: `url(${gambar})`}}>
+                                <div className="image-content-features">
 
-                                    <div className="row text-image-content">
+                                    <div className="text-image-content ">
                                         test test test
                                     </div>
                                 </div>
-                                <div className="row">
+                                <div className="row ps-2">
                                     
-                                    <div className="col-12">
+                                    <div className="col-12 mt-3">
                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, totam.
                                     </div>
 
-                                    <div className="col-12">
+                                    <div className="col-12  mt-3">
                                         <div className="outer-bar-features">
                                             <div className="inner-bar-features"></div>
                                         </div>
                                     </div>
 
-                                    <div className="col-11">
+                                    <div className="col-11 mt-2">
                                         <div className="row justify-content-between set-data-per-features">
-                                            <div className="col-2 text-start"> 75% </div>
-                                            <div className="col-2 text-end"> $1000.000</div>
+                                            <div className="col-2 text-start set-pecents"> 75% </div>
+                                            <div className="col-2 text-end set-price"> $1000.000</div>
                                         </div>
                                     </div>
                                 </div>
@@ -144,18 +147,162 @@ const IntroLP = () => {
                             </div>
 
                         </div>
-                        <div className="col-12 col-md-3 ">
+                        
+                        <div className="col-12 col-md-4 mt-5">
                             <div className="box-content-features">
-                                    p
+                                <div className="image-content-features">
+                                    <div className="text-image-content ">
+                                        test test test
+                                    </div>
+                                    </div>
+                                    <div className="row ps-2">
+
+                                        <div className="col-12 mt-3">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, totam.
+                                        </div>
+
+                                        <div className="col-12  mt-3">
+                                            <div className="outer-bar-features">
+                                                <div className="inner-bar-features"></div>
+                                            </div>
+                                        </div>
+
+                                        <div className="col-11 mt-2">
+                                            <div className="row justify-content-between set-data-per-features">
+                                                <div className="col-2 text-start set-pecents"> 75% </div>
+                                                <div className="col-2 text-end set-price"> $1000.000</div>
+                                            </div>
+                                        </div>
                                 </div>
-                        </div>
-                        <div className="col-12 col-md-3 ">
-                            <div className="box-content-features">
-                                p
                             </div>
                         </div>
 
+                        <div className="col-12 col-md-4 mt-5 ">
+                            <div className="box-content-features">
+                                <div className="image-content-features">
+                                    <div className="text-image-content ">
+                                        test test test
+                                    </div>
+                                </div>
+                                <div className="row ps-2">
 
+                                    <div className="col-12 mt-3">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, totam.
+                                    </div>
+
+                                    <div className="col-12  mt-3">
+                                        <div className="outer-bar-features">
+                                            <div className="inner-bar-features"></div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-11 mt-2">
+                                        <div className="row justify-content-between set-data-per-features">
+                                            <div className="col-2 text-start set-pecents"> 75% </div>
+                                            <div className="col-2 text-end set-price"> $1000.000</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4 mt-5 ">
+
+                            <div className="box-content-features">
+                                <div className="image-content-features">
+
+                                    <div className="text-image-content ">
+                                        test test test
+                                    </div>
+                                </div>
+                                <div className="row ps-2">
+                                    
+                                    <div className="col-12 mt-3">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, totam.
+                                    </div>
+
+                                    <div className="col-12  mt-3">
+                                        <div className="outer-bar-features">
+                                            <div className="inner-bar-features"></div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-11 mt-2">
+                                        <div className="row justify-content-between set-data-per-features">
+                                            <div className="col-2 text-start set-pecents"> 75% </div>
+                                            <div className="col-2 text-end set-price"> $1000.000</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+                        <div className="col-12 col-md-4 mt-5">
+                            <div className="box-content-features">
+                                <div className="image-content-features">
+                                    <div className="text-image-content ">
+                                        test test test
+                                    </div>
+                                    </div>
+                                    <div className="row ps-2">
+
+                                        <div className="col-12 mt-3">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, totam.
+                                        </div>
+
+                                        <div className="col-12  mt-3">
+                                            <div className="outer-bar-features">
+                                                <div className="inner-bar-features"></div>
+                                            </div>
+                                        </div>
+
+                                        <div className="col-11 mt-2">
+                                            <div className="row justify-content-between set-data-per-features">
+                                                <div className="col-2 text-start set-pecents"> 75% </div>
+                                                <div className="col-2 text-end set-price"> $1000.000</div>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-md-4 mt-5 ">
+                            <div className="box-content-features">
+                                <div className="image-content-features">
+                                    <div className="text-image-content ">
+                                        test test test
+                                    </div>
+                                </div>
+                                <div className="row ps-2">
+
+                                    <div className="col-12 mt-3">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, totam.
+                                    </div>
+
+                                    <div className="col-12  mt-3">
+                                        <div className="outer-bar-features">
+                                            <div className="inner-bar-features"></div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-11 mt-2">
+                                        <div className="row justify-content-between set-data-per-features">
+                                            <div className="col-2 text-start set-pecents"> 75% </div>
+                                            <div className="col-2 text-end set-price"> $1000.000</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                      
+
+
+                    </div>
+                    <div className="row mt-5 justify-content-center">
+                        <div className="col-3 text-center">
+                            <button className="view-more-button">View More</button>
+                        </div>
                     </div>
                 </div>
 
@@ -163,8 +310,8 @@ const IntroLP = () => {
             </div>
 
             
-            <br />
 
+            <Footer/>
         </>
     )
 }
