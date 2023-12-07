95e3c6f5404bbbc4183e08b90f997f929653f0b2
JorLojor <nugas123man@gmail.com>
2023-12-08 03:04:25 +0700

    p

--- FE/src/pages/BookingPage/BookingPageStepOne.jsx
+++ FE/src/pages/BookingPage/BookingPageStepOne.jsx
@@ -1,8 +1,33 @@
+
+
 function BookingPageStepOne() {
+
     
     return(
         <>
-        
+        <div className ="container-fluid text-center title-header">
+            Crowd<span>Lab.</span>
+        </div>
+        <div className ="container-fluid">
+            <div className ="row justify-content-center">
+                <div className ="col-4 set-bk-title text-center">
+                    Yay! Completed
+                </div>
+            </div>
+        </div>
+        <div className ="container-fluid">
+                <div className ="row justify-content-center">
+                <div className ="col-8 set-bk-title text-center">
+                    <img src="" alt=""/>
+                    
+                </div>
+            </div>
+            <div className ="row justify-content-center">
+                <div className ="col-2">
+                    <button className  ="btn-find-ap">Continue</button>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
