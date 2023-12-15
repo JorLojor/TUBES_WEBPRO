e1ea2c39792a866b467dce0d7759ee76b5c03b60
JorLojor <nugas123man@gmail.com>
2023-12-16 00:16:31 +0700

    resolve domisili

--- FE/src/pages/LandingPage/DiscoverProject/DiscoverProject.jsx
+++ FE/src/pages/LandingPage/DiscoverProject/DiscoverProject.jsx
@@ -1,45 +1,44 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import propTypes from 'prop-types';
 
-const DiscoverProject = () => {
+const DiscoverProject = ({ data }) => {
+    const imgUrl = ''
 
+    console.log(data);
     return (
         <>
             <div className="set-row set-row-trird">
-            <div className="row justify-content-between pe-5">
-                <div className="col-1 set-categori-textt"><span>Category</span></div>
-                
-            </div>
-            <div className="row justify-content-between set-inner-row">
-                <div className="col-4 text-discover ">
-                    Discover Project <br/>
-                    By Category
+                <div className="row justify-content-between pe-5">
+                    <div className="col-1 set-categori-textt"><span>Category</span></div>
                 </div>
-                <div className="col-8">
-                    <div className="row  justify-content-between">
+                <div className="row justify-content-between set-inner-row">
+                    <div className="col-4 text-discover ">
+                        Discover Project <br />
+                        By Category
+                    </div>
+                    <div className="col-8">
+                        <div className="row  justify-content-between">
+                        {data.map((item, index) => {
+                            return (
+                                <div key={index} className="col-3">
+                                    <img src={`../../../../../BE/Upload/`} alt="" className="img-fluid" />
+                                    {item.title}
+                                    {item.description}
+                                </div>
+                            );
+                        })}
 
-                        <div className="col-3  ">
-                            <div className="box-content-discover">
-                                
-                            </div>
                         </div>
-                        <div className="col-3 ">
-                            <div className="box-content-discover">
-                                
-                            </div>
-                        </div>
-                        <div className="col-3">
-                            <div className="box-content-discover">
-                                
-                            </div>
-                        </div>
-                        
-
                     </div>
                 </div>
-
             </div>
-                </div>
         </>
     )
+}
+
+DiscoverProject.propTypes = {
+    data: propTypes.array
 }
 
 export default DiscoverProject;
--- FE/src/pages/LandingPage/IntroLP/intro.jsx
+++ FE/src/pages/LandingPage/IntroLP/intro.jsx
@@ -1,30 +1,34 @@
+/* eslint-disable no-unused-vars */
 import './intro.css';
 import Footer from '../../../components/footer/footer';
 import MainContent from '../MainContent/MainContent';
 import MainDataLP from '../MainDataLP/MainDataLP';
 import DiscoverProject from '../DiscoverProject/DiscoverProject';
 import FeaturedProjectLP from '../FeaturedProject/FeaturedProjectLP';
-
-
-
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 const IntroLP = () => {
-
+    const [dataDP, setDataDP] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:3777/api/project')
+        .then((res) => {
+            setDataDP(res.data.projects.slice(0,3));
+            console.log(res.data.projects)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }, []);
     return(
         <>
             <div className="container-intro-lp">
                 <MainContent/>
                 <MainDataLP/>
-                <DiscoverProject/>
+                <DiscoverProject data={dataDP}/>
                 <FeaturedProjectLP/>
-
-
             </div>
-
-            
-
             <Footer/>
         </>
     )
 }
-
 export default IntroLP;
