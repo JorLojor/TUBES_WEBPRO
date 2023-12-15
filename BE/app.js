65b922a85396fcdafa20625f8e9f25b8a61b830e
JorLojor <nugas123man@gmail.com>
2023-12-16 00:34:13 +0700

    seeep

--- BE/app.js
+++ BE/app.js
@@ -1,44 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const api = require('./api/index');
 require('dotenv').config();
 
-
+// Middleware CORS
 app.use(cors());
-app.use('../Upload', express.static(path.join(__dirname, 'Upload')));
+
+// Middleware Static untuk Upload
+app.use('/get-img', express.static(path.join(__dirname, 'Upload'))); // http://localhost:5000/get-img/1620114470799-10000000.jpg
+
+// URL Koneksi MongoDB
 const url = 'mongodb+srv://nugas123man:Telkom.0329@tubeswebpro.fprfgno.mongodb.net/?retryWrites=true&w=majority';
 
-// database conection
-mongoose.connect(url,{
+// Database Connection
+mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    console.log('Connected to the Database successfully')
+    console.log('Connected to the Database successfully');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
 });
 
+// Middleware Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// body parser
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// API Routes
+app.use('/api', api);
 
-
-// api
-app.use('/api', api)
-// api
-
-// error handler
+// Error Handler
 app.use((err, req, res, next) => {
     res.status(500).json({
         success: false,
-        request : req.body,
-        next : next.api,
+        request: req.body,
+        next: next.api,
         message: err.message,
         data: null,
     });
 });
-// error handler
-
 
 module.exports = app;
