6fc2ee0d72918d4e7800c6c34e03d113d0f5f761
JorLojor <nugas123man@gmail.com>
2023-12-29 23:54:47 +0700

    seeeeeep

--- FE/src/pages/AboutPage/OurGroup/OurGroup.jsx
+++ FE/src/pages/AboutPage/OurGroup/OurGroup.jsx
@@ -1,4 +1,11 @@
 import './OurGroup.css';
+// img
+import hanip from '../../../assets/hanip.png';
+import jorss from '../../../assets/jorss.png';
+import geo from '../../../assets/geo.png';
+import pute from '../../../assets/pute.png';
+import rama from '../../../assets/rama.png';
+
 
 
 const OurGroup = () => {
@@ -12,39 +19,39 @@ const OurGroup = () => {
                     </h1>
                     <div className="col-4 mt-5">
                         <div className="box-poto m-auto">
-
+                            <img src={hanip} alt="" />
                         </div>
-                        <h3>geore ibrahim hanif</h3>
-                        <p>apaakek</p>
+                        <h3>hanif fikri</h3>
+                        <p>FE & HORE TEAM & style react</p>
                     </div>
 
                     <div className="col-4 mt-5">
                         <div className="box-poto m-auto">
-
+                            <img src={jorss} alt="" />
                         </div>
                         <h3>geore ibrahim hanif</h3>
-                        <p>apaakek</p>
+                        <p>BE / FE</p>
                     </div>
                     <div className="col-4 mt-5">
                         <div className="box-poto m-auto">
-
+                            <img src={geo} alt="" />
                         </div>
-                        <h3>geore ibrahim hanif</h3>
-                        <p>apaakek</p>
+                        <h3>geonanda</h3>
+                        <p>FE component react</p>
                     </div>
                     <div className="col-4 mt-5">
                         <div className="box-poto m-auto">
-
+                            <img src={pute} alt="" />
                         </div>
-                        <h3>geore ibrahim hanif</h3>
-                        <p>apaakek</p>
+                        <h3>Putrie Risky</h3>
+                        <p>FE pages & component</p>
                     </div>
                     <div className="col-4 mt-5">
                         <div className="box-poto m-auto">
-
+                            <img src={rama} alt="" />
                         </div>
-                        <h3>geore ibrahim hanif</h3>
-                        <p>apaakek</p>
+                        <h3>Ramadhani Dwi</h3>
+                        <p>UI UX & progress report</p>
                     </div>
 
                 </div>
